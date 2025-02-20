5b64d192235d13b7825627390b1da3c080754975
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-02-20 17:53:51 +0530

    Push - L1-Pattern Printing III

---
+++ Assignmet Submission/JavaScript/Introduction to Git and Github/Q4-L1_Pattern_Printing_III.js
@@ -0,0 +1,34 @@
+/**
+ * FTSDM01S2_2508 Introduction to Git and Github
+ * 
+ * Title: L1-Pattern Printing III
+
+Problem Statement:
+
+You are given a number stored in a variable with the name N You have to print the pattern as shown below according to the value stored in N For example, consider the value stored in N = 5, then the required pattern will be.
+
+* * * * *
+
+*
+
+*
+
+*
+
+* * * * *
+
+
+*/
+let n=5
+let bag="";
+for(i=0;i<n;i++){
+  for(j=0;j<n;j++){
+    if(i==0 || j==0 || i==n-1){
+
+      bag += "* ";
+    }
+  }
+  bag +="\n"
+}
+
+console.log(bag)
