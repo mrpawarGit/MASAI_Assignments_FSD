bf47227adc97a2e898fcb5355a0e5b22778f8296
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-02-20 17:54:58 +0530

    Push - L1-Pattern Printing III

--- Assignmet Submission/JavaScript/Introduction to Git and Github/Q4-L1_Pattern_Printing_III.js
+++ Assignmet Submission/JavaScript/Introduction to Git and Github/Q4-L1_Pattern_Printing_III.js
@@ -8,13 +8,9 @@ Problem Statement:
 You are given a number stored in a variable with the name N You have to print the pattern as shown below according to the value stored in N For example, consider the value stored in N = 5, then the required pattern will be.
 
 * * * * *
-
 *
-
 *
-
 *
-
 * * * * *
 
 
