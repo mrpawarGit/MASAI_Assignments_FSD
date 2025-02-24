7d785830e4d60560906f321ae8ec19fee2574ecf
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-02-25 02:22:24 +0530

    New Pattern Added

--- Assignmet Submission/JavaScript/Multidimensional Arrays (M1S2)/Q14_Pattern3.js
+++ Assignmet Submission/JavaScript/Multidimensional Arrays (M1S2)/Q14_Pattern3.js
@@ -1,22 +1,3 @@
-/**
- 
-Q: 14
-**Title:**L1-Pattern Printing III
-
-**Problem Statement:**You are given a number stored in a variable with the name N You have to print the pattern as shown below according to the value stored in N For example, consider the value stored in N = 5, then the required pattern will be.
-
-* * * * *
-
-*
-
-*
-
-*
-
-* * * * *
-
-
- */
 
 function patternPrint(N){
 
