ab2ec5c6ab2da5d2146ffc240b41aa2cbd9d6421
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-02-27 00:43:07 +0530

    Asg2

---
+++ Assignmet Submission/JavaScript/Objects/Q11.js
@@ -0,0 +1,17 @@
+/**
+
+Q: 11
+Title: L0 - Traversing Objects with for...in
+
+Problem Statement: You are given an object representing a book with properties such as title, author, and year. Your task is to use the for...in loop to traverse through the object and print each key along with its associated value in the format: key: value.
+
+Example Input:
+
+let book = { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 };
+
+Expected Output:
+
+title: The Hobbit author: J.R.R. Tolkien year: 1937
+
+Submission Guidelines: Please submit your masai git directory link.
+ */
