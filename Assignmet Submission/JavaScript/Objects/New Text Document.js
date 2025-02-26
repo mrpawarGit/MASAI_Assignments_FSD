7c9014e0937aa92752115d6a98e125e02f9f5dfe
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-02-26 23:31:27 +0530

    Objects

---
+++ Assignmet Submission/JavaScript/Objects/New Text Document.js
@@ -0,0 +1,19 @@
+/**
+ 
+Title: L0 - Determine Senior Citizen Discount Eligibility
+
+Problem Statement: Write a program that checks if a person is eligible for a senior citizen discount based on their age. If the age is 60 or more, the output should be "Eligible for Senior Discount". Otherwise, the output should be "Not Eligible for Senior Discount". Use the ternary operator for this task. Additionally, handle an edge case where the age provided is a negative number or 0. For such cases, output "Invalid Age".
+
+Example:
+
+let age = 65;
+
+Output: "Eligible for Senior Discount"
+
+let age = -1;
+
+Output: "Invalid Age"
+
+Submission Guidelines: Please submit your Masai Git directory link.
+
+ */
