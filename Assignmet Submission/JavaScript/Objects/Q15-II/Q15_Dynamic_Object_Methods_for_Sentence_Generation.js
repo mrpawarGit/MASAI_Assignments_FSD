9475d88571f7615c4b51042f669992f3402a0abe
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-08 21:23:40 +0530

    Update Q15_Dynamic_Object_Methods_for_Sentence_Generation.js

--- Assignmet Submission/JavaScript/Objects/Q15-II/Q15_Dynamic_Object_Methods_for_Sentence_Generation.js
+++ Assignmet Submission/JavaScript/Objects/Q15-II/Q15_Dynamic_Object_Methods_for_Sentence_Generation.js
@@ -1,6 +1,5 @@
 /**
-
-Q: 15
+Q: 4
 Title:
 L2 - Dynamic Object Methods for Sentence Generation
 
@@ -21,96 +20,93 @@ Example:
 Initial Object Structure:
 
 let sentenceBuilder = {
+
 subject: "I",
+
 verb: "am",
+
 object: "coding",
+
 buildSentence: function() { ... },
+
 updateProperty: function(property, value) { ... } };
 
 Example 1:
+
 sentenceBuilder.buildSentence();
 
 Expected Output:
+
 "I am coding"
 
 Example 2:
+
 sentenceBuilder.updateProperty("verb", "am learning"); sentenceBuilder.buildSentence();
 
 Expected Output:
+
 "I am learning coding"
 
 Example 3:
+
 sentenceBuilder.updateProperty("subject", "The cat");
+
 sentenceBuilder.buildSentence();
 
 Expected Output:
+
 "The cat is learning coding"
 
 Example 4:
+
 sentenceBuilder.updateProperty("mood", "happy");
 
 Expected Output:
+
 "Invalid property"
 
 Example 5:
+
 sentenceBuilder.updateProperty("verb", ""); sentenceBuilder.buildSentence();
 
 Expected Output:
-"Incomplete sentence"
 
+"Incomplete sentence"
  */
 
 let sentenceBuilder = {
-  subject : "I",
-  verb : "am",
-  object : "coding",
-  
-  buildSentence: function(){
-    let sentence = this.subject && this.verb && this.object 
-    ? console.log(`${this.subject} ${this.verb} ${this.object}`)
-    :console.log("Incomplete sentence");
 
-    //return sentence;
+  subject: "I",
+  verb: "am",
+  object: "coding",
+
+  buildSentence: function () {
+    if(this.subject=="" || this.verb==""||this.object==""){
+      console.log("Incomplete sentence")
+      return
+    }
+    let str = `${this.subject} ${this.verb} ${this.object}`;
+    console.log(str)
   },
 
-  updateProperty : function(property, value){
-    if(["subject","verb","object"].includes(property)){
-      //this[property] = value;
-      sentenceBuilder[property] = value;
+  updateProperty: function(property, value) {
+    if(property!="subject" && property!="verb" && property!="object"){
+      console.log("Invalid property")
     }
-    else{
-      console.log("Invalid property");
-    }
+    sentenceBuilder[property]=value
+  } 
+};
 
-  }
-}
+sentenceBuilder.buildSentence();  
 
-//Example 1: //Expected Output: "I am coding" 
-sentenceBuilder.buildSentence()
-
-//Example 2: //Expected Output: "I am learning coding"
 sentenceBuilder.updateProperty("verb", "am learning"); 
 sentenceBuilder.buildSentence();
 
-//Example 3: //Expected Output: "The cat is learning coding"
+
 sentenceBuilder.updateProperty("subject", "The cat");
 sentenceBuilder.buildSentence();
 
-//Example 4: //Expected Output: "Invalid property"
-sentenceBuilder.updateProperty("mood", "happy");
-
-//Example 5: //Expected Output: "Incomplete sentence"
 sentenceBuilder.updateProperty("verb", ""); 
 sentenceBuilder.buildSentence();
 
-
-
-
-
-
-
-
-
-
-
-
+sentenceBuilder.updateProperty("mood", "happy");
