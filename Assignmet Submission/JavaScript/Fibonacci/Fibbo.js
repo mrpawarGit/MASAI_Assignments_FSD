4409d45037103237a63abf8c4ff43209f119168c
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-09 13:20:30 +0530

    fibbo

--- Assignmet Submission/JavaScript/Fibonacci/Fibbo.js
+++ Assignmet Submission/JavaScript/Fibonacci/Fibbo.js
@@ -1,7 +1,6 @@
 // program to generate fibonacci series up to n terms
 
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
+
 
 console.log('Fibonacci Series:');
 
