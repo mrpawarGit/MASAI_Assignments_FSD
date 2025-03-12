a2e4408dc501dd83ec83c4a3c95d683452e483d7
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-12 16:25:48 +0530

    sports academy

---
+++ Assignmet Submission/JavaScript/M2-S1  Coding Assignment/Q3_SportsAcademy.js
@@ -0,0 +1,46 @@
+/**
+Q: 3
+Problem Statement
+A sports academy tracks athlete details, including name, sport, scores, and personal details. You need to extract and manipulate data using destructuring and the spread operator.
+
+Dataset:
+const athletes = [
+  { name: "Virat", sport: "Cricket", scores: [89, 75, 102], age: 35 },
+  { name: "Sindhu", sport: "Badminton", scores: [21, 19, 25], age: 29 },
+  { name: "Neeraj", sport: "Javelin", scores: [88, 90, 85], age: 26 }
+];
+
+Tasks:
+1️⃣ Extract the first athlete's name and sport using destructuring.
+2️⃣ Destructure the scores of Neeraj, storing the first two separately and the rest in another variable.
+3️⃣ Create a new object for Neeraj using the spread operator, updating his age to 27 and adding worldChampion: true.
+4️⃣ Merge all scores from all athletes into a single array using the spread operator.
+
+Expected Output:
+"Virat plays Cricket"
+88, 90, [85]
+{ name: "Neeraj", sport: "Javelin", scores: [88, 90, 85], age: 27, worldChampion: true }
+[89, 75, 102, 21, 19, 25, 88, 90, 85]
+
+ */
+
+const athletes = [
+  { name: "Virat", sport: "Cricket", scores: [89, 75, 102], age: 35 },
+  { name: "Sindhu", sport: "Badminton", scores: [21, 19, 25], age: 29 },
+  { name: "Neeraj", sport: "Javelin", scores: [88, 90, 85], age: 26 }
+];
+
+//1️ -Extract the first athlete's name and sport using destructuring.
+
+let [{name,sport}]=athletes;
+
+//2 -Destructure the scores of Neeraj, storing the first two separately and the rest in another variable.
+let [,,{scores:[s1,s2,s3]}] = athletes
+let arr = [s3];
+
+//3 -Create a new object for Neeraj using the spread operator, updating his age to 27 and adding worldChampion: true.
+
+//outputs
+console.log(`${name} plays ${sport}`); //1
+
+console.log(s1,s2,arr); //2
