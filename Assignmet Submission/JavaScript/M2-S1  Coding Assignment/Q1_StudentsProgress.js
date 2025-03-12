d6939fd52c9e0b8892bfab812d3fe0c288ce8a62
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-12 12:41:05 +0530

    studentsProgress

---
+++ Assignmet Submission/JavaScript/M2-S1  Coding Assignment/Q1_StudentsProgress.js
@@ -0,0 +1,30 @@
+/**
+Q: 1 Online Learning Platform - Course Completion Analysis
+
+Problem Statement:
+An online learning platform tracks students' progress.
+
+Requirements:
+const studentsProgress = [
+  { name: "Ramesh", completedLessons: 40, totalLessons: 50 },
+  { name: "Sita", completedLessons: 25, totalLessons: 50 },
+  { name: "Manoj", completedLessons: 30, totalLessons: 60 },
+  { name: "Pooja", completedLessons: 48, totalLessons: 50 },
+  { name: "Anil", completedLessons: 15, totalLessons: 50 }
+];
+
+Implement the following logic:
+-Filter students who have completed at least 50% of the course.
+-Map to calculate their completion percentage.
+-Reduce to find the average completion percentage of all active students.
+-Filter students who have more than 80% completion to get a list of top learners.
+
+Expected Output:
+-List of active students with their completion percentage.
+-The average course completion percentage.
+
+Submission Guidelines
+Implement the solution and ensure that it runs without errors.
+Include comments in your code to explain the logic and flow.
+Submit your GitHub directory Link.
+ */
