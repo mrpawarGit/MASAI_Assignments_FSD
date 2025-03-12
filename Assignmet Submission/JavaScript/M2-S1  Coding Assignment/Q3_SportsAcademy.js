a39fc3f3e8a2acc337cbc2ffeabf5a6ea8849835
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-12 16:44:50 +0530

    sports academy

--- Assignmet Submission/JavaScript/M2-S1  Coding Assignment/Q3_SportsAcademy.js
+++ Assignmet Submission/JavaScript/M2-S1  Coding Assignment/Q3_SportsAcademy.js
@@ -39,8 +39,20 @@ let [,,{scores:[s1,s2,s3]}] = athletes
 let arr = [s3];
 
 //3 -Create a new object for Neeraj using the spread operator, updating his age to 27 and adding worldChampion: true.
+let [,,{name:n1,sport:sp,scores:sc,age}]=athletes
+const neeraj = {"name":n1,"sport":sp,"score":sc,"age":27};
+
+//4 -Merge all scores from all athletes into a single array using the spread operator
+let [{scores}] = athletes
+let [,{scores:sc2}] = athletes
+let [,,{scores:sc3}] = athletes
+let mixScore = [...scores,...sc2,...sc3]
 
 //outputs
 console.log(`${name} plays ${sport}`); //1
 
 console.log(s1,s2,arr); //2
+
+console.log(neeraj) //3
+
+console.log(mixScore) //4
