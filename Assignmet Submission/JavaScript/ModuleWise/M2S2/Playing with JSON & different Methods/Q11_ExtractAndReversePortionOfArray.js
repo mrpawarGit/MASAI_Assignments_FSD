71d64498cb25f18900b3d5464fcde438b6b4b29d
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-20 00:37:13 +0530

    Extract and Reverse a Portion of an Array

--- Assignmet Submission/JavaScript/ModuleWise/M2S2/Playing with JSON & different Methods/Q11_ExtractAndReversePortionOfArray.js
+++ Assignmet Submission/JavaScript/ModuleWise/M2S2/Playing with JSON & different Methods/Q11_ExtractAndReversePortionOfArray.js
@@ -1,7 +1,5 @@
 /*
-Q: 11
-Title:
-L0 - Extract and Reverse a Portion of an Array
+Q.11 - Extract and Reverse a Portion of an Array
 
 Problem Statement:
 Given an array of integers [15, 30, 45, 60, 75, 90], write a function extractAndReverse 
@@ -13,3 +11,13 @@ that performs the following tasks:
 3. The original array should remain unchanged. The function should return the reversed 
    subarray.
 */
+
+function newArray(arr){
+   return arr.slice(2,4).reverse();
+}
+
+let arr = [15, 30, 45, 60, 75, 90];
+let arr2 = newArray(arr)
+
+console.log(arr);
+console.log(arr2);
