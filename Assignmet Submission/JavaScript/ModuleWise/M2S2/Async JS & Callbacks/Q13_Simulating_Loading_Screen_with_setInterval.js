af2a09f0351881f3d3789a1a4548a1fa91801794
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-03-22 14:56:25 +0530

    Simulating a Loading Screen with setInterval

---
+++ Assignmet Submission/JavaScript/ModuleWise/M2S2/Async JS & Callbacks/Q13_Simulating_Loading_Screen_with_setInterval.js
@@ -0,0 +1,20 @@
+/*
+Q: 13 
+Title: Simulating a Loading Screen with setInterval
+
+Problem Statement:
+Create a JavaScript program that simulates a loading screen using setInterval. The program should:
+  1.Start by logging "Loading..." every 1 second.
+  2.After 5 seconds, stop the loading messages and log "Loaded successfully!".
+
+Use setInterval to repeat the loading message and clearInterval to stop it after 5 seconds.
+*/
+
+const loading = setInterval(()=>{
+  console.log("Loading...")
+},1000)
+
+setTimeout(()=>{
+  clearInterval(loading)
+  console.log("Loaded successfully!");
+},5000)
