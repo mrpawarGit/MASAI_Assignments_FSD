4f8e43566b1f29f5f8b2c598bba90678771e29ec
mayur <mrpawar5798@gmail.com>
2025-03-27 15:29:55 +0530

    Fetch and Display Users

--- Assignmet Submission/JavaScript/ModuleWise/M2S3/AsyncAwait & Intro to Fetch/Q8_Fetch and Display Products with Error Handling.js
+++ Assignmet Submission/JavaScript/ModuleWise/M2S3/AsyncAwait & Intro to Fetch/Q8_Fetch and Display Products with Error Handling.js
@@ -17,3 +17,44 @@ Key Requirements:
        But make sure that you're using reduce method to do so , if not then the assignment 
        will be flagged as Not - Submitted
  */
+
+// fetch('https://fakestoreapi.com/products')
+// .then(response=>response.json())
+// // .then(response=>response.forEach(element => {
+// //    console.log(element.category)
+// // }))
+// .then(response=>console.log(response))
+
+async function dispProducts(){
+   
+
+   try{
+      let fetching = await fetch('https://fakestoreapi.com/products')
+
+      if(!fetching.ok){
+         throw new Error("Unable to Fetch");
+      }
+
+      let products = await fetching.json()
+      //all products
+      //console.log("All Products:",products)
+
+      //all products details
+      products.forEach((product)=>{
+         console.log("Title:",product.title)
+         console.log("Price:",product.price)
+         console.log("Image:",product.image)
+      })
+      
+      // total price of all products category wise
+      
+      
+   }
+   catch(error){
+      //console.log("Failed to fetch products.Please try again later")
+      console.error("Failed to fetch products.Please try again later")
+   }
+
+}
+
+dispProducts();
