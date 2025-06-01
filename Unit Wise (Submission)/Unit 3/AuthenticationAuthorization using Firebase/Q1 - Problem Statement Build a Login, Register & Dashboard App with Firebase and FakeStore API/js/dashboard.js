06b9a84b714811d0439883eaf9aa8cd087935834
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-02 02:20:37 +0530

    fake-store-auth

--- Unit Wise (Submission)/Unit 3/AuthenticationAuthorization using Firebase/Q1 - Problem Statement Build a Login, Register & Dashboard App with Firebase and FakeStore API/js/dashboard.js
+++ Unit Wise (Submission)/Unit 3/AuthenticationAuthorization using Firebase/Q1 - Problem Statement Build a Login, Register & Dashboard App with Firebase and FakeStore API/js/dashboard.js
@@ -1,10 +1,29 @@
 import { auth } from "../firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   onAuthStateChanged(auth, (user) => {
-    
-    document.getElementById('username').innerHTML = user.email
-      
+    document.getElementById("username").innerHTML = user.email;
+
+    fetchProducts();
   });
 });
+
+async function fetchProducts() {
+  let res = await fetch("https://fakestoreapi.com/products");
+  let data = await res.json();
+  let conatiner = document.getElementById("conatiner");
+  conatiner.innerHTML = "";
+
+  data.forEach((product) => {
+    let div = document.createElement("div");
+    div.className = "product-box";
+
+    div.innerHTML = `
+      <h4>${product.title}</h4>
+      <img src="${product.image}" alt="${product.title}">
+      <p>Price: ${product.price}</p>
+    `;
+    conatiner.appendChild(div);
+  });
+}
