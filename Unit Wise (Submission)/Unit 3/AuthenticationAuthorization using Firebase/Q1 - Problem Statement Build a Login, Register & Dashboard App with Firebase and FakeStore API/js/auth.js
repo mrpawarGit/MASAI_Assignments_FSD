a049138fc0732ef954c1005bddb47b33ec339181
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-02 00:25:52 +0530

    fake-store-auth

--- Unit Wise (Submission)/Unit 3/AuthenticationAuthorization using Firebase/Q1 - Problem Statement Build a Login, Register & Dashboard App with Firebase and FakeStore API/js/auth.js
+++ Unit Wise (Submission)/Unit 3/AuthenticationAuthorization using Firebase/Q1 - Problem Statement Build a Login, Register & Dashboard App with Firebase and FakeStore API/js/auth.js
@@ -4,3 +4,22 @@ import {
   signInWithEmailAndPassword,
   signOut
 } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+
+document.addEventListener('DOMContentLoaded',()=>{
+  const loginBtn = document.getElementById('login-btn');
+  const signupBtn = document.getElementById('signup-btn');
+  const logoutBtn = document.getElementById('logout-btn');
+
+  if(loginBtn){
+    loginBtn.addEventListener('click',async()=>{
+      const email = document.getElementById('login-email').value;
+      const password = document.getElementById('login-password').value;
+      try {
+        await 
+      } catch (error) {
+        
+      }
+    })
+  }
+})
+
