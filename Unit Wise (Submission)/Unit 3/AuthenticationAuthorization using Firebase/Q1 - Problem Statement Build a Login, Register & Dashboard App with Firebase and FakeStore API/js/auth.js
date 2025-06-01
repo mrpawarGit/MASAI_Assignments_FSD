14f40b0c2a21f68b5dc6a9f43585f919388101b1
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-02 01:02:40 +0530

    fake-store-auth

--- Unit Wise (Submission)/Unit 3/AuthenticationAuthorization using Firebase/Q1 - Problem Statement Build a Login, Register & Dashboard App with Firebase and FakeStore API/js/auth.js
+++ Unit Wise (Submission)/Unit 3/AuthenticationAuthorization using Firebase/Q1 - Problem Statement Build a Login, Register & Dashboard App with Firebase and FakeStore API/js/auth.js
@@ -2,24 +2,51 @@ import { auth } from "../firebase-config.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
 } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
-document.addEventListener('DOMContentLoaded',()=>{
-  const loginBtn = document.getElementById('login-btn');
-  const signupBtn = document.getElementById('signup-btn');
-  const logoutBtn = document.getElementById('logout-btn');
+document.addEventListener("DOMContentLoaded", () => {
+  const loginBtn = document.getElementById("login-btn");
+  const signupBtn = document.getElementById("signup-btn");
+  const logoutBtn = document.getElementById("logout-btn");
 
-  if(loginBtn){
-    loginBtn.addEventListener('click',async()=>{
-      const email = document.getElementById('login-email').value;
-      const password = document.getElementById('login-password').value;
+  // login function
+  if (loginBtn) {
+    loginBtn.addEventListener("click", async () => {
+      const email = document.getElementById("login-email").value;
+      const password = document.getElementById("login-password").value;
       try {
-        await 
+        await signInWithEmailAndPassword(auth, email, password);
+        // redirect users to login page
+        window.location.href = "dashboard.html";
       } catch (error) {
-        
+        document.getElementById("login-msg").innerText = error.message;
       }
-    })
+    });
   }
-})
 
+  if (signupBtn) {
+    signupBtn.addEventListener("click", async () => {
+      const email = document.getElementById("signup-email").value;
+      const password = document.getElementById("signup-password").value;
+      try {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        // redirect users to login page
+        window.location.href = "index.html";
+      } catch (error) {
+        document.getElementById("signup-msg").innerText = error.message;
+      }
+    });
+  }
+
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", async () => {
+      await signOut(auth)
+      window.location.href = "index.html";
+    });
+  }
+});
