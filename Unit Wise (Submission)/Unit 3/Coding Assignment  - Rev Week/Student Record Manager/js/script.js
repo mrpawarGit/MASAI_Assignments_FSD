7b430ccfd82b19bc1c8f73d158575ff414bd4978
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-04 23:57:19 +0530

    Student Record Manager

--- Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Student Record Manager/js/script.js
+++ Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Student Record Manager/js/script.js
@@ -2,16 +2,24 @@ let api = `https://studentdb-701b6-default-rtdb.asia-southeast1.firebasedatabase
 
 async function fetchData() {
   let res = await fetch(api);
-  let students = await res.json()
-  let tbody = document.getElementById('tbody')
-  tbody.innerHTML = ""
+  let students = await res.json();
+  let tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
 
-  students.forEach(student=>{
-    let div = document.createElement('tbody')
-    div.className = 'student-row';
-    
+  students.forEach((student) => {
+    let tr = document.createElement("tr");
+    tr.className = "student-row";
 
-  })
+    tr.innerHTML = `
+        <td>${student.Name}</td>
+        <td>${student.Batch}</td>
+        <td>${student.Age}</td>
+        <td>${student.Score}</td>
+        <td>Edit/Delete</td>
+    `;
+
+    tbody.appendChild(tr)
+  });
 }
 
 addEventListener("DOMContentLoaded", () => fetchData());
