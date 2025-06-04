b36c4ba4b136cfce0894c2e1d3313be45127506d
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-04 23:48:31 +0530

    Student Record Manager

--- Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Student Record Manager/js/script.js
+++ Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Student Record Manager/js/script.js
@@ -1,6 +1,10 @@
+let api = `https://studentdb-701b6-default-rtdb.asia-southeast1.firebasedatabase.app/students.json`;
 
 async function fetchData() {
+  let res = await fetch(api);
+  let data = await res.json()
 
+  console.log(data)
 }
 
 addEventListener("DOMContentLoaded", () => fetchData());
