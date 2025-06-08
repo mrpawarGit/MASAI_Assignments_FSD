9144c0b005a9b2ed4098c1cdd52863e683c03f0b
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-08 16:28:05 +0530

    Task Manager App with Firebase Authentication

---
+++ Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Task Manager App with Firebase Authentication/js/dashboard.js
@@ -0,0 +1,8 @@
+import { auth } from "../firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  onAuthStateChanged(auth, (user) => {
+    document.getElementById("username").innerHTML = user.email;
+  });
+});
