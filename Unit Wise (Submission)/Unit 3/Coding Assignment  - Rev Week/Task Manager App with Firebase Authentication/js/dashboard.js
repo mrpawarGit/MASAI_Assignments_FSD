56e3a618ee2bd1b93ae12782e824098181292508
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-09 00:06:57 +0530

    Task Manager App with Firebase Authentication

--- Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Task Manager App with Firebase Authentication/js/dashboard.js
+++ Unit Wise (Submission)/Unit 3/Coding Assignment  - Rev Week/Task Manager App with Firebase Authentication/js/dashboard.js
@@ -15,4 +15,72 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     document.getElementById("user-name").innerHTML = name.toUpperCase();
   });
+
+  fetchData();
 });
+
+const api = "https://jsonplaceholder.typicode.com/posts";
+let posts = [];
+
+async function fetchData() {
+  let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  posts = await res.json();
+
+  dispData(posts);
+}
+
+function dispData(data) {
+  let postContainer = document.getElementById("post-container");
+  postContainer.innerHTML = "";
+
+  data.forEach((user) => {
+    let div = document.createElement("div");
+    div.className = "post-box";
+
+    div.innerHTML = `
+      <p>${user.title}</p>
+      <button onclick="editPost(${user.id})">Edit</button>
+      <button onclick="deletePost(${user.id})">Delete</button>
+    `;
+
+    postContainer.appendChild(div);
+  });
+}
+
+// delete
+window.deletePost = async function (id) {
+  const response = await fetch(`${api}/${id}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    posts = posts.filter((post) => post.id !== id);
+    dispData(posts);
+  } else {
+    console.error("Failed to delete post");
+  }
+};
+
+// edit
+window.editPost = async function (id) {};
+
+// post
+let i = 101;
+
+window.addPost = async function () {
+  let userInput = document.getElementById("userInput").value.trim();
+  if (!userInput) return;
+
+  await fetch(api, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: 0,
+      id: 0,
+      title: userInput,
+      body: "NA",
+    }),
+  });
+
+  document.getElementById("userInput").value = "";
+  dispData(posts);
+};
