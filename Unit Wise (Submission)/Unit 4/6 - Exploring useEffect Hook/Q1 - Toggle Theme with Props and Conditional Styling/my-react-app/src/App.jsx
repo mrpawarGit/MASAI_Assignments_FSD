471677774590e17cf60786e77bbb19f9eadf3287
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-28 16:04:11 +0530

    Theme-Toggler

--- Unit Wise (Submission)/Unit 4/6 - Exploring useEffect Hook/Q1 - Toggle Theme with Props and Conditional Styling/my-react-app/src/App.jsx
+++ Unit Wise (Submission)/Unit 4/6 - Exploring useEffect Hook/Q1 - Toggle Theme with Props and Conditional Styling/my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Paragraphs from "./components/Paragraphs";
 
 function App() {
   const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    console.log("Current Theme:", theme);
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
