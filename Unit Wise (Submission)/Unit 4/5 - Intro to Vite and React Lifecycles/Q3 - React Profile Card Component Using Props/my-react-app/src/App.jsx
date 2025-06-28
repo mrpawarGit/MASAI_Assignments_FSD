9d51eda4811cd721ec00978c56be51b38dd01c9c
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-29 00:26:22 +0530

    ProfileCard

--- Unit Wise (Submission)/Unit 4/5 - Intro to Vite and React Lifecycles/Q3 - React Profile Card Component Using Props/my-react-app/src/App.jsx
+++ Unit Wise (Submission)/Unit 4/5 - Intro to Vite and React Lifecycles/Q3 - React Profile Card Component Using Props/my-react-app/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import React from "react";
 import ProfileCard from "./components/ProfileCard";
 
 function App() {
+  const styleJ = {
+    justifyContent: "center",
+    left: "40%",
+    position: "relative",
+  };
+
   return (
-    <>
-      <ProfileCard />
-    </>
+    <div style={styleJ}>
+      <h1 style={{ textAlign: "center" }}>User Profiles</h1>
+
+      <ProfileCard
+        name="John Cena"
+        age={29}
+        bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, suscipit vitae sint ipsum eos rem. Dolorum quae neque in numquam temporibus, enim doloribus soluta deserunt autem, dicta cupiditate, quas ipsam?"
+      />
+
+      <ProfileCard age={26} />
+    </div>
   );
 }
 
--- Unit Wise (Submission)/Unit 4/5 - Intro to Vite and React Lifecycles/Q3 - React Profile Card Component Using Props/my-react-app/src/components/ProfileCard.jsx
+++ Unit Wise (Submission)/Unit 4/5 - Intro to Vite and React Lifecycles/Q3 - React Profile Card Component Using Props/my-react-app/src/components/ProfileCard.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-function ProfileCard() {
+function shortBio(text, maxLength = 100) {
+  if (text.length <= maxLength) return text;
+  return text.slice(0, maxLength) + "… Read More";
+}
+
+function ProfileCard({
+  name = "Monaco GG",
+  age,
+  bio = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, suscipit vitae sint ipsum eos rem. Dolorum quae neque in numquam temporibus, enim doloribus soluta deserunt autem, dicta cupiditate, quas ipsam?",
+}) {
   return (
-    <div>
-      <h1>ProfileCard</h1>
+    <div style={card}>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>{shortBio(bio)}</p>
     </div>
   );
 }
+
+const card = {
+  // textAlign: "center",
+  padding: "20px",
+  margin: "10px",
+  border: "2px solid",
+};
 
 export default ProfileCard;
