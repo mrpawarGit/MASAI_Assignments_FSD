a94c1722c7d5a7f291c01396275aa180aae53fb1
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-28 23:24:38 +0530

    Quote Generator

--- Unit Wise (Submission)/Unit 4/6 - Exploring useEffect Hook/Q2 - Daily Quote Generator (Auto-refresh + Button)/my-react-app/src/components/Quote.jsx
+++ Unit Wise (Submission)/Unit 4/6 - Exploring useEffect Hook/Q2 - Daily Quote Generator (Auto-refresh + Button)/my-react-app/src/components/Quote.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Quote() {
+  const [quote, setQuote] = useState([]);
+
+  async function fetchData() {
+    const res = await fetch("http://api.quotable.io/random");
+    const data = await res.json();
+
+    setQuote(data);
+  }
+
+  useState(() => {
+    fetchData();
+
+    let auto = setInterval(() => {
+      fetchData();
+    }, 30000);
+
+    return () => clearInterval(auto);
+  }, []);
+
   return (
     <>
-      <h2>Daily Quote</h2>
+      <div>
+        <h2>Daily Quote Generator</h2>
+        <div key={quote.id}>
+          <p>"{quote.content}"</p>
+          <p>- {quote.author}</p>
+        </div>
+        <button onClick={fetchData}>Get New</button>
+      </div>
     </>
   );
 }
