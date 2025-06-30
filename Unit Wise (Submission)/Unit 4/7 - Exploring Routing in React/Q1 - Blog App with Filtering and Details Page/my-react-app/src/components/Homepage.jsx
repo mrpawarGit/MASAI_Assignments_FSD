a9c46d7412b1d1a0d88f4d894e93dcb9dbe171e9
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-30 23:43:42 +0530

    React

--- Unit Wise (Submission)/Unit 4/7 - Exploring Routing in React/Q1 - Blog App with Filtering and Details Page/my-react-app/src/components/Homepage.jsx
+++ Unit Wise (Submission)/Unit 4/7 - Exploring Routing in React/Q1 - Blog App with Filtering and Details Page/my-react-app/src/components/Homepage.jsx
@@ -10,17 +10,35 @@ function Homepage() {
   async function fetchData() {
     const res = await fetch("https://dummyjson.com/posts");
     const data = await res.json();
-
     setPosts(data.posts);
   }
 
+  const postContainer = {
+    margin: "20px",
+    padding: "15px",
+    display: "grid",
+    gridTemplateColumns: "repeat(4,1fr)",
+    gap: "20px",
+  };
+
+  const postBox = {
+    border: "2px solid",
+    borderRadius: "8px",
+    padding: "20px",
+    cursor: "pointer",
+  };
+
   return (
-    <>
-      <div>
-        <h2>Posts</h2>
-        
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Posts</h1>
+      <div style={postContainer}>
+        {posts.map((post) => (
+          <div key={post.id} style={postBox}>
+            <h5>{post.title}</h5>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
