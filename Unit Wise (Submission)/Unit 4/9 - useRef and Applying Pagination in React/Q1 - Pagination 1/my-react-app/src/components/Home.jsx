77f533806265767e7e3778616f91aff6a66307ca
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-06 20:39:08 +0530

    React

--- Unit Wise (Submission)/Unit 4/9 - useRef and Applying Pagination in React/Q1 - Pagination 1/my-react-app/src/components/Home.jsx
+++ Unit Wise (Submission)/Unit 4/9 - useRef and Applying Pagination in React/Q1 - Pagination 1/my-react-app/src/components/Home.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Home(){
-  return(
+export default function Home() {
+  const [charcters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (page == 0) setPage(1);
+    fetchData();
+  }, [page]);
+
+  async function fetchData() {
+    const res = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
+    const data = await res.json();
+    // console.log(data.results);
+    setCharacters(data.results);
+  }
+
+  return (
     <div>
-    <h2>Rickand & Morty</h2>
+      <h2>Rickand & Morty</h2>
+      {charcters.map((character) => (
+        <div key={character.id}>
+          <img src={character.image} />
+          <h4>{character.name}</h4>
+        </div>
+      ))}
+      <button onClick={() => setPage((prev) => prev - 1)} disabled={page == 1}>
+        Prev
+      </button>
+      <button onClick={() => setPage((prev) => prev + 1)}>Next</button>
     </div>
-  )
+  );
 }
