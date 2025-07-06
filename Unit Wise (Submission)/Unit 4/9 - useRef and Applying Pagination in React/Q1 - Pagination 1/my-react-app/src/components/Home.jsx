5d1a18118283cc7cbccf2c260b1f4dcc4060c9bb
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-06 22:08:17 +0530

    React

--- Unit Wise (Submission)/Unit 4/9 - useRef and Applying Pagination in React/Q1 - Pagination 1/my-react-app/src/components/Home.jsx
+++ Unit Wise (Submission)/Unit 4/9 - useRef and Applying Pagination in React/Q1 - Pagination 1/my-react-app/src/components/Home.jsx
@@ -18,16 +18,35 @@ export default function Home() {
     setCharacters(data.results);
   }
 
+  const charbox = {
+    border: "1px solid",
+    padding: "20px",
+    display: "inline-block",
+    margin: "10px",
+    borderRadius: "8px",
+  };
+
+  const imgbox = {
+    height: "200px",
+    width: "200px",
+    border: "1px solid",
+  };
+
   return (
     <div>
-      <h2>Rickand & Morty</h2>
+      <h1>Rickand & Morty</h1>
       {charcters.map((character) => (
-        <div key={character.id}>
-          <img src={character.image} />
+        <div key={character.id} style={charbox}>
+          <img src={character.image} style={imgbox} />
           <h4>{character.name}</h4>
         </div>
       ))}
-      <button onClick={() => setPage((prev) => prev - 1)} disabled={page == 1}>
+      <br />
+      <button
+        onClick={() => setPage((prev) => prev - 1)}
+        disabled={page == 1}
+        style={{ marginRight: "20px" }}
+      >
         Prev
       </button>
       <button onClick={() => setPage((prev) => prev + 1)}>Next</button>
