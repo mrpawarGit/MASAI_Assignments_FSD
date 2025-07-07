24829f3130263f60ae33edd4a86a1b481cc30ae0
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-07 21:00:31 +0530

    React

--- Unit Wise (Submission)/Unit 4/9 - useRef and Applying Pagination in React/Q2 - Pagination 2/my-react-app/src/components/Home.jsx
+++ Unit Wise (Submission)/Unit 4/9 - useRef and Applying Pagination in React/Q2 - Pagination 2/my-react-app/src/components/Home.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 function Home() {
   const [lists, setLists] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  
+  const pageRef = useRef([]);
 
   // for 10 items
   const itemsPerPage = 10;
@@ -17,10 +17,25 @@ function Home() {
   const pageNum = Array.from({ length: totalPages }, (ele, i) => i + 1);
   // console.log(pageNum);
 
-
   useEffect(() => {
     getData();
   }, []);
+
+  // to focus on 1st page on first load or reload
+  useEffect(() => {
+    if (pageRef.current[0]) {
+      pageRef.current[0].focus();
+    }
+  }, [lists]);
+
+  // for foucs when page changed
+  useEffect(() => {
+    const currentBtn = pageRef.current[currentPage - 1];
+    if (currentBtn) {
+      currentBtn.focus();
+    }
+    console.log("Foucs changed");
+  }, [currentPage]);
 
   async function getData() {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
@@ -36,10 +51,11 @@ function Home() {
           <li key={list.id}>{list.title}</li>
         ))}
       </ul>
-      {pageNum.map((num) => (
+      {pageNum.map((num, index) => (
         <button
           key={num}
           onClick={() => setCurrentPage(num)}
+          ref={(el) => (pageRef.current[index] = el)}
           style={{
             margin: "5px",
             backgroundColor: num == currentPage ? "green" : "grey",
