9fa916cefe72527d5e65ca25bc860cf37dcb18fd
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-08 23:20:52 +0530

    REact

--- Unit Wise (Submission)/Unit 4/10 - Advanced State Management Exploring useReducer/Q6 - Advanced State Management Exploring useReducer/my-react-app/src/App.jsx
+++ Unit Wise (Submission)/Unit 4/10 - Advanced State Management Exploring useReducer/Q6 - Advanced State Management Exploring useReducer/my-react-app/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState = {
+  theme: "light",
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reducer(state, action) {
+  switch (action.type) {
+    case "TOGGLE_THEME":
+      return {
+        ...state,
+        theme: state.theme === "light" ? "dark" : "light",
+      };
+    default:
+      return state;
+  }
 }
 
-export default App
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const dark = {
+    backgroundColor: "black",
+    color: "white",
+  };
+
+  const light = {
+    backgroundColor: "white",
+    color: "black",
+  };
+
+  return (
+    <div style={state.theme === "dark" ? dark : light}>
+      <h1>Theme: {state.theme}</h1>
+      <button onClick={() => dispatch({ type: "TOGGLE_THEME" })}>
+        Change Theme
+      </button>
+    </div>
+  );
+}
