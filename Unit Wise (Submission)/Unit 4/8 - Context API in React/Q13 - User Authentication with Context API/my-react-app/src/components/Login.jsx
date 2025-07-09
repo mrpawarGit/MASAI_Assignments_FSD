13695098fddf6e5366e5b39ef2dce1dd6c2f7503
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-09 19:51:58 +0530

    UseContext

--- Unit Wise (Submission)/Unit 4/8 - Context API in React/Q13 - User Authentication with Context API/my-react-app/src/components/Login.jsx
+++ Unit Wise (Submission)/Unit 4/8 - Context API in React/Q13 - User Authentication with Context API/my-react-app/src/components/Login.jsx
@@ -5,10 +5,13 @@ export default function Login() {
   const { user, setUser } = useContext(UserContext);
   const [isLog, setIsLog] = useState(false);
 
+  const btnColor = {
+    backgroundColor: isLog ? "red" : "green",
+  };
   return (
     <div>
       <h2>{isLog ? `Hello, ${user}` : "Login Please"}</h2>
-      <button onClick={() => setIsLog((prev) => !prev)}>
+      <button onClick={() => setIsLog((prev) => !prev)} style={btnColor}>
         {isLog ? "Logout" : "Login"}
       </button>
     </div>
