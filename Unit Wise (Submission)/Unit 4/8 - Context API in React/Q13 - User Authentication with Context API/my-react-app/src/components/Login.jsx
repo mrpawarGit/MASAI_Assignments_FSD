b3880b67bca3bc6c8cecd9386ea7fc2b3f2f7a50
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-09 19:46:52 +0530

    UseContext

--- Unit Wise (Submission)/Unit 4/8 - Context API in React/Q13 - User Authentication with Context API/my-react-app/src/components/Login.jsx
+++ Unit Wise (Submission)/Unit 4/8 - Context API in React/Q13 - User Authentication with Context API/my-react-app/src/components/Login.jsx
@@ -5,14 +5,12 @@ export default function Login() {
   const { user, setUser } = useContext(UserContext);
   const [isLog, setIsLog] = useState(false);
 
-  const handleLoginToggle = () => {
-    setIsLog((prev) => !prev);
-  };
-
   return (
     <div>
       <h2>{isLog ? `Hello, ${user}` : "Login Please"}</h2>
-      <button onClick={handleLoginToggle}>{isLog ? "Logout" : "Login"}</button>
+      <button onClick={() => setIsLog((prev) => !prev)}>
+        {isLog ? "Logout" : "Login"}
+      </button>
     </div>
   );
 }
