d3460968324b1b0242f123f20a34d88c91196939
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-14 15:26:16 +0530

    REacApp

--- Unit Wise (Submission)/Unit 4/13 - Modern Redux with Redux Toolkit/Q1 - Shopping Cart application/my-react-app/src/components/Cart.jsx
+++ Unit Wise (Submission)/Unit 4/13 - Modern Redux with Redux Toolkit/Q1 - Shopping Cart application/my-react-app/src/components/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeItem } from "../features/cartSlice";
+import { removeItem } from "../cart/cartSlice";
 import Item from "./Item";
 // import "../index.css"
 
--- Unit Wise (Submission)/Unit 4/13 - Modern Redux with Redux Toolkit/Q1 - Shopping Cart application/my-react-app/src/components/ProductList.jsx
+++ Unit Wise (Submission)/Unit 4/13 - Modern Redux with Redux Toolkit/Q1 - Shopping Cart application/my-react-app/src/components/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { addItem } from "../features/cartSlice";
+import { addItem } from "../cart/cartSlice";
 import Item from "./Item";
 
 const products = [
