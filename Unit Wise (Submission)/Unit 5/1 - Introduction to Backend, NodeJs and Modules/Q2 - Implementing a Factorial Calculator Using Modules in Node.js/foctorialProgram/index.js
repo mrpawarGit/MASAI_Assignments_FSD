1c5640b0b24a70fd41aded70b089e6620d9d7efe
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-24 01:10:38 +0530

    Implementing a Factorial Calculator Using Modules in Node.js

---
+++ Unit Wise (Submission)/Unit 5/1 - Introduction to Backend, NodeJs and Modules/Q2 - Implementing a Factorial Calculator Using Modules in Node.js/foctorialProgram/index.js
@@ -0,0 +1,5 @@
+const factorial = require("./factorial");
+
+console.log("Factorial of 5:", factorial(5));
+console.log("Factorial of 7:", factorial(7));
+console.log("Factorial of 10:", factorial(10));
