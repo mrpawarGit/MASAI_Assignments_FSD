c03ae2404cf50337f9f1bd707ef54d36e55cf5dd
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-25 20:15:45 +0530

    boxen demo

--- Unit Wise (Submission)/Unit 5/2 - Introduction to Package Managers, npm, and Express in Node.js/Q1 - Exploring & Implementimng First External NPM Module in Node.js/boxenDemo/index.js
+++ Unit Wise (Submission)/Unit 5/2 - Introduction to Package Managers, npm, and Express in Node.js/Q1 - Exploring & Implementimng First External NPM Module in Node.js/boxenDemo/index.js
@@ -3,10 +3,4 @@ const boxen = require("boxen");
 let title = "Hurray!!!";
 let msg = "I am using my first external module!";
 
-console.log(
-  boxen(`${title}\n${msg}`, {
-    padding: 1,
-    margin: 1,
-    borderStyle: "double",
-  })
-);
+console.log(boxen(msg, { title: title, titleAlignment: "center" }));
