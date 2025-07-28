f6ef6d1379d649fe856235db527e7ec117b7e5a6
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-28 13:04:14 +0530

    EventEmitter

--- Unit Wise (Submission)/Unit 5/2 - Introduction to Package Managers, npm, and Express in Node.js/Q5 - Events/eventsModule/index.js
+++ Unit Wise (Submission)/Unit 5/2 - Introduction to Package Managers, npm, and Express in Node.js/Q5 - Events/eventsModule/index.js
@@ -1,12 +1,45 @@
 const express = require("express");
 const app = express();
 
+const emitter = require("./eventLogger");
+const delayedResponse = require("./delay");
+
 app.get("/", (req, res) => {
   res.send("Server Stated");
 });
 
 app.get("/test", (req, res) => {
   res.send("Test route is working!");
+});
+
+// emiter -> /emit?message=HelloWorld
+app.get("/emit", (req, res) => {
+  const { message } = req.query;
+
+  if (!message) {
+    return res
+      .status(400)
+      .json({ error: "Message query parameter is required" });
+  }
+
+  emitter.emit("log", message);
+  res.json({ status: "Event emitted", message });
+});
+
+// Delay route -> /delay?message=WaitForIt&time=3000
+app.get("/delay", async (req, res) => {
+  const { message, time } = req.query;
+
+  if (!message || !time) {
+    return res
+      .status(400)
+      .json({ error: "Both message and time query parameters are required" });
+  }
+
+  const delay = parseInt(time);
+
+  const result = await delayedResponse(message, delay);
+  res.json({ delayedMessage: result, delay: `${delay}ms` });
 });
 
 app.listen(3000, () => {
