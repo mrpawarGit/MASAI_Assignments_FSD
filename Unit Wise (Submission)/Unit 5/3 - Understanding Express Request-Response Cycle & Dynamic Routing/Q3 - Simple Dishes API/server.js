e54fe07618340c43f35cc1731745947d1a9451d9
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-29 17:55:50 +0530

    Simple Dishes API

--- Unit Wise (Submission)/Unit 5/3 - Understanding Express Request-Response Cycle & Dynamic Routing/Q3 - Simple Dishes API/server.js
+++ Unit Wise (Submission)/Unit 5/3 - Understanding Express Request-Response Cycle & Dynamic Routing/Q3 - Simple Dishes API/server.js
@@ -10,6 +10,13 @@ app.post("/add", (req, res) => {
   res.send("Data Added..");
 });
 
+// route for reading file
+app.get("/all-dishes", (req, res) => {
+  let data = fs.readFileSync("./db.json", "utf-8");
+  console.log(data);
+  res.send(data);
+});
+
 // default handle
 app.use((req, res) => {
   res.status(404).json({ error: "404 Not Found" });
