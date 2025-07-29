96b43f836305f41c9966cedba40a5e3fa6b44950
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-07-29 16:45:53 +0530

    Express App with User Routes

--- Unit Wise (Submission)/Unit 5/3 - Understanding Express Request-Response Cycle & Dynamic Routing/Q2 - Building a Simple Express App with User Routes/server.js
+++ Unit Wise (Submission)/Unit 5/3 - Understanding Express Request-Response Cycle & Dynamic Routing/Q2 - Building a Simple Express App with User Routes/server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+const users = [
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Doe", email: "jane@example.com" },
+  { id: 3, name: "Bob Smith", email: "bob@example.com" },
+];
+
+// list
+app.get("/users/list", (req, res) => {
+  res.status(200).json(users);
+});
+
+// 1st user
+app.get("/users/get", (req, res) => {
+  res.status(200).json(users[0]);
+});
+
+// 404 Not found
 app.use((req, res) => {
-  res.status(404).send("Not Found !!");
+  res.status(404).json({ error: "404 Not Found" });
 });
 
 app.listen(3000, () => {
