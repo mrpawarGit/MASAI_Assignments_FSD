e3b104ddf66c0f568dff9d90c71867949aa5a467
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-08-13 14:34:26 +0530

    ratelimiter

--- Unit Wise (Submission)/Unit 5/5 - External Middlewares, Database Fundamentals and MongoDB CRUD/Q1 - Implementing Rate Limiting in Express/index.js
+++ Unit Wise (Submission)/Unit 5/5 - External Middlewares, Database Fundamentals and MongoDB CRUD/Q1 - Implementing Rate Limiting in Express/index.js
@@ -6,6 +6,19 @@ const app = express();
 // middlewere
 app.use(express.json());
 
+// limit logic
+const limit = ratelimit({
+  windowMs: 1 * 60 * 1000, // 1 min
+  limit: 5, // 5 req
+  message: "Max req reached, wait for 1 min...",
+});
+
+// public
+app.use("/api/public", apiRoutes.publicRouter);
+
+// limitd
+app.use("/api/limited", limit, apiRoutes.limitedRouter);
+
 // undefined routes
 app.use((req, res) => {
   res.json({ msg: "Req Not Found" });
