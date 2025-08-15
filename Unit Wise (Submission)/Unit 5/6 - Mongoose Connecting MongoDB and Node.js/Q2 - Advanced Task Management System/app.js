0597229cea5106a5743d82f1f9de4e66427f61d3
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-08-15 23:19:50 +0530

    adv TaskModel

--- Unit Wise (Submission)/Unit 5/6 - Mongoose Connecting MongoDB and Node.js/Q2 - Advanced Task Management System/app.js
+++ Unit Wise (Submission)/Unit 5/6 - Mongoose Connecting MongoDB and Node.js/Q2 - Advanced Task Management System/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const connectToDB = require("./config/db");
+const TaskRouter = require("./routes/task.routes");
 
 const app = express();
 app.use(express.json());
 connectToDB();
+
+// route for tasks
+app.use("/", TaskRouter);
 
 // invalid handle
 app.use((req, res) => {
