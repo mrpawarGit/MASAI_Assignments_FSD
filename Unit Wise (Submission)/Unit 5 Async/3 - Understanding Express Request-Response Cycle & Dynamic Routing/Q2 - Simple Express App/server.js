9074e915232a55aac20607c58a2e7cd6216d1df4
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-09-04 13:00:20 +0530

    Simple Express App

---
+++ Unit Wise (Submission)/Unit 5 Async/3 - Understanding Express Request-Response Cycle & Dynamic Routing/Q2 - Simple Express App/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/users/get", (req, res) => {
+  res.json({ id: 1, name: "John Doe", email: "john@example.com" });
+});
+
+app.get("/users/list", (req, res) => {
+  res.json([
+    { id: 1, name: "John Doe", email: "john@example.com" },
+    { id: 2, name: "Jane Doe", email: "jane@example.com" },
+    { id: 3, name: "Bob Smith", email: "bob@example.com" },
+  ]);
+});
+
+app.use((req, res) => {
+  res.json({ error: "404 Not Found" });
+});
+app.listen(3000, () => {
+  console.log("Server Started at 3000");
+});
