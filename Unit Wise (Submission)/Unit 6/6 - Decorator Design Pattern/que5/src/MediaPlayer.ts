03302d565baf3abddcea575633f159244d44a51a
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-10-13 20:05:37 +0530

    Decorator Design Pattern

---
+++ Unit Wise (Submission)/Unit 6/6 - Decorator Design Pattern/que5/src/MediaPlayer.ts
@@ -0,0 +1,42 @@
+interface MediaFile {
+  play(): void;
+}
+
+class AudioFile implements MediaFile {
+  play(): void {
+    console.log("Playing audio file...");
+  }
+}
+
+class VideoFile implements MediaFile {
+  play(): void {
+    console.log("Playing video file...");
+  }
+}
+
+class PDFFile implements MediaFile {
+  play(): void {
+    console.log("Displaying PDF document...");
+  }
+}
+
+class MediaPlayer {
+  private media: MediaFile;
+
+  constructor(media: MediaFile) {
+    this.media = media;
+  }
+
+  playMedia(): void {
+    this.media.play();
+  }
+}
+
+const audioPlayer = new MediaPlayer(new AudioFile());
+audioPlayer.playMedia();
+
+const videoPlayer = new MediaPlayer(new VideoFile());
+videoPlayer.playMedia();
+
+const pdfPlayer = new MediaPlayer(new PDFFile());
+pdfPlayer.playMedia();
