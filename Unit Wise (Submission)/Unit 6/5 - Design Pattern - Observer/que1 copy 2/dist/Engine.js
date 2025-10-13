8894e9c3ddee57a622580bd19ac94345792caf47
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-10-13 19:51:50 +0530

    Replace Class with Interface

---
+++ Unit Wise (Submission)/Unit 6/5 - Design Pattern - Observer/que1 copy 2/dist/Engine.js
@@ -0,0 +1,22 @@
+"use strict";
+var Engine = /** @class */ (function () {
+    function Engine() {
+    }
+    Engine.prototype.start = function () {
+        console.log("Engine started");
+    };
+    return Engine;
+}());
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = new Engine();
+    }
+    Car.prototype.drive = function () {
+        this.engine.start();
+        console.log("Car is driving");
+    };
+    return Car;
+}());
+// Demonstration
+var car = new Car();
+car.drive();
