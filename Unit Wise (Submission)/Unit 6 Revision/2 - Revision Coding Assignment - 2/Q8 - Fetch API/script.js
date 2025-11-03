968e50eabae1a66c59a05f363e7e522eeedac62f
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-11-03 16:28:04 +0530

    dummy

--- Unit Wise (Submission)/Unit 6 Revision/2 - Revision Coding Assignment - 2/Q8 - Fetch API/script.js
+++ Unit Wise (Submission)/Unit 6 Revision/2 - Revision Coding Assignment - 2/Q8 - Fetch API/script.js
@@ -1,6 +1,6 @@
 const api_url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-function handleSearch() {
+async function handleSearch() {
   const searchTxt = document.getElementById("searchTxt").value;
   searchByName(searchTxt);
 }
@@ -9,10 +9,40 @@ async function searchByName(mealName) {
   try {
     const res = await fetch(`${api_url}${mealName}`);
     const data = await res.json();
-    console.log(data);
+    renderData(data.meals);
   } catch (err) {
     console.error("Error fetching meals:", err);
   }
 }
 
-async function renderData() {}
+searchByName("a");
+
+function renderData(meals) {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+
+  if (!meals) {
+    container.innerHTML = `<h3>No Meals Found</h3>`;
+    return;
+  }
+
+  meals.forEach((meal) => {
+    const div = document.createElement("div");
+    div.className = "foodbox";
+
+    div.innerHTML = `
+      <div style="
+        border:1px solid #aaa;
+        width:200px;
+        padding:10px;
+        margin:10px;
+        border-radius:8px;
+      ">
+        <img src="${meal.strMealThumb}" width="180" style="border-radius:8px"/>
+        <h3>${meal.strMeal}</h3>
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+}
