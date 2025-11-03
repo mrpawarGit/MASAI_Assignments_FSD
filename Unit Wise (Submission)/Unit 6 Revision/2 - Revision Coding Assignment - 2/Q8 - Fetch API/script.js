05e3fad7210761d8c57fefd8232c0280de9d4f0a
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-11-03 16:54:32 +0530

    dummy

--- Unit Wise (Submission)/Unit 6 Revision/2 - Revision Coding Assignment - 2/Q8 - Fetch API/script.js
+++ Unit Wise (Submission)/Unit 6 Revision/2 - Revision Coding Assignment - 2/Q8 - Fetch API/script.js
@@ -1,27 +1,42 @@
 const api_url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let allMeals = [];
+
+function sortAll(order) {
+  if (order == "asc") {
+    allMeals = allMeals.sort((a, b) =>
+      a.strMeal.toLowerCase().localeCompare(b.strMeal.toLowerCase())
+    );
+  } else {
+    allMeals = allMeals.sort((a, b) =>
+      b.strMeal.toLowerCase().localeCompare(a.strMeal.toLowerCase())
+    );
+  }
+
+  renderData(allMeals);
+}
 
 async function handleSearch() {
   const searchTxt = document.getElementById("searchTxt").value;
-  searchByName(searchTxt);
-}
-
-async function searchByName(mealName) {
-  try {
-    const res = await fetch(`${api_url}${mealName}`);
-    const data = await res.json();
-    renderData(data.meals);
-  } catch (err) {
-    console.error("Error fetching meals:", err);
-  }
+  const res = await fetch(`${api_url}${searchTxt}`);
+  const data = await res.json();
+  allMeals = data.meals || [];
+  renderData(allMeals);
 }
 
 searchByName("a");
+
+async function searchByName(mealName) {
+  const res = await fetch(`${api_url}${mealName}`);
+  const data = await res.json();
+  allMeals = data.meals || [];
+  renderData(allMeals);
+}
 
 function renderData(meals) {
   const container = document.getElementById("container");
   container.innerHTML = "";
 
-  if (!meals) {
+  if (!meals || meals.length === 0) {
     container.innerHTML = `<h3>No Meals Found</h3>`;
     return;
   }
@@ -31,18 +46,29 @@ function renderData(meals) {
     div.className = "foodbox";
 
     div.innerHTML = `
-      <div style="
-        border:1px solid #aaa;
-        width:200px;
-        padding:10px;
-        margin:10px;
-        border-radius:8px;
-      ">
-        <img src="${meal.strMealThumb}" width="180" style="border-radius:8px"/>
-        <h3>${meal.strMeal}</h3>
-      </div>
+      <img src="${meal.strMealThumb}" />
+      <h3>${meal.strMeal}</h3>
+      <p>${meal.strCategory}</p>
     `;
 
     container.appendChild(div);
   });
+}
+
+function filterMeals(type) {
+  if (!allMeals) return;
+
+  if (type === "veg") {
+    const vegMeals = allMeals.filter(
+      (meal) => meal.strCategory === "Vegetarian"
+    );
+    renderData(vegMeals);
+  } else {
+    const nonVegMeals = allMeals.filter((meal) =>
+      ["Beef", "Chicken", "Goat", "Lamb", "Pork", "Seafood"].includes(
+        meal.strCategory
+      )
+    );
+    renderData(nonVegMeals);
+  }
 }
